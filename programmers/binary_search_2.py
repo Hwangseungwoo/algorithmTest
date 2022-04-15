258fb2ff37e27f7063aeab0800232429ea3ae0ca
Hwangseungwoo <simodda@yonsei.ac.kr>
2022-04-16 00:14:58 +0900

    프로그래머스 고득점 kit 이분탐색 2번

--- programmers/binary_search_2.py
+++ programmers/binary_search_2.py
@@ -1,24 +1,33 @@
 def solution(distance, rocks, n):
     answer = 0
-    mid = distance//2
     rocks.sort()
-    for i in range(len(rocks)):
-        for j in range(i+1, len(rocks)):
-            new_rocks = rocks[:]
-            first = new_rocks[i]
-            second = new_rocks[j]
-            new_rocks.remove(first)
-            new_rocks.remove(second)
-            dis = []
-            for k in range(len(new_rocks)+1):
-                if k==0:
-                    dis.append(new_rocks[k])
-                elif k==len(new_rocks):
-                    dis.append(distance-new_rocks[len(new_rocks)-1])
-                else:
-                    dis.append(new_rocks[k]-new_rocks[k-1])
-            mini = min(dis)
-            if mini>answer:
-                answer = mini
+    left = 1
+    right = distance
+    
+    
+    while left<=right:
+        mid = (left+right) // 2
+        cur = 0
+        cnt = 0
+        
+        for i in range(len(rocks)):
+            # 만약 거리가 더 길거나 같으면 내가 설정한 최소값에 문제 없으므로 그 다리 유지하고 다음과의 거리 체크
+            if rocks[i]-cur>=mid:
+                cur = rocks[i]
+            # 만약 거리가 더 작으면 내가 설정한 최소값이랑 다르니까 없애는 목록 +1
+            else:
+                cnt += 1
+        # 최종목적지랑 마지막돌이랑 거리도 확인
+        if distance-cur<mid:
+            cnt += 1
+        
+        # 만약 돌 없앤게 더 많으면 내가 설정한 최소값이 너무 큰거니까 왼쪽 이분 탐색
+        if cnt>n:
+            right = mid -1
+        # 만약 돌 없앤거랑 같거나 작으면 최소값이 너무 작으니까 오른쪽 이분 탐색
+        else:
+            answer = mid
+            left = mid +1
+    
                 
     return answer
